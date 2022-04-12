ce672d8fcec04adbde513d4653715a8845d28ba3
Matias <matias@karplus.com.ar>
2022-04-12 09:09:37 -0300

    Walking out of spaces finally fucking works

--- DSVJ2 - Camerawork/Assets/Scripts/CameraWork.cs
+++ DSVJ2 - Camerawork/Assets/Scripts/CameraWork.cs
@@ -7,12 +7,22 @@ public class CameraWork : MonoBehaviour
     [SerializeField] private Transform playerCam;
     public LayerMask terrainLayer;
     public Material newMat;
+    public Material oldMat;
+    private List<RaycastHit> roofHits = new List<RaycastHit>();
     public void Update()
     {
         if (Physics.Linecast(this.transform.position, playerCam.position, out RaycastHit hitInfo, terrainLayer))
         {
             hitInfo.collider.gameObject.GetComponent<MeshRenderer>().material = newMat;
-        } 
+            roofHits.Add(hitInfo);
+        }
+
+        if (roofHits.Count > 0 && !Physics.Linecast(this.transform.position, playerCam.position, out RaycastHit hits, terrainLayer)) {
+            for (int i = 0; i < roofHits.Count; i++)
+            {
+                roofHits[i].collider.gameObject.GetComponent<MeshRenderer>().material = oldMat;
+            }
+        }
     }
  }
 
